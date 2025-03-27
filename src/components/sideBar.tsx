e6843b61e419f80299a2b403e06ad3d2e8cd81cb
이의진 <aghpt04@gmail.com>
2025-03-27 09:05:21 +0900

    fix :: sidebar

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -27,17 +27,33 @@ export const SideBar = ({closed, setClosed}: SideBarProps) => {
                         </HeadWrap>
                     }
                 </NavBox>
-                <NavBox onClick={() => setShowBar(prev => !prev)}>
-                    <img src={Subject} alt="" />
+                <SubjectNavBox>
                     <SubjectWrapper>
-                    {!closed && 
-                        <HeadWrap>
-                            <p>과목</p>
-                            <ArrowIcon $isOpen={showBar} src={Arrow} alt=">"/>
-                        </HeadWrap>
-                    }
+                        <Wrap onClick={() => setShowBar(prev => !prev)}>
+                            <img src={Subject} alt="" />
+                            {!closed && 
+                                <HeadWrap>
+                                    <p>과목</p>
+                                    <ArrowIcon $isOpen={showBar} src={Arrow} alt=">"/>
+                                </HeadWrap>
+                            }
+                        </Wrap>
+                        <SubWrap $isOpen={showBar}>
+                            <SubjectName>
+                                <p>국어</p>
+                            </SubjectName>
+                            <SubjectName>
+                                <p>수학</p>
+                            </SubjectName>
+                            <SubjectName>
+                                <p>한국사</p>
+                            </SubjectName>
+                            <SubjectName>
+                                <p>과학</p>
+                            </SubjectName>
+                        </SubWrap>
                     </SubjectWrapper>
-                </NavBox>
+                </SubjectNavBox>
             </NavWrapper>
         </Wrapper>
     )
@@ -80,6 +96,7 @@ const HeadWrap = styled.div`
     align-items: center;
     width: 100%;
     justify-content: space-between;
+    cursor: pointer;
     > p {
         font-size: 15px;
     }
@@ -94,6 +111,48 @@ const SubjectWrapper = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+    gap: 17px;
+`;
+
+const SubjectNavBox = styled.div`
+    display: flex;
+    margin-left: 8px;
+`;
+
+const Wrap = styled.div`
+    display: flex;
+    width: 100%;
     justify-content: space-between;
+    gap: 17px;
+`;
+
+const SubWrap = styled.div<{$isOpen: boolean}>`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin-left: 30px;
+    overflow: hidden;
+    
+    height: ${({ $isOpen }) => ($isOpen ? "120px" : "0px")};  
+    opacity: ${({ $isOpen }) => ($isOpen ? "1" : "0")};  
+    transform: translateY(${({ $isOpen }) => ($isOpen ? "0" : "-10px")});
+    
+    visibility: ${({ $isOpen }) => ($isOpen ? "visible" : "hidden")};  
+    transition: height 0.2s ease-out, opacity 0.2s ease-out, transform 0.2s ease-out, visibility 0.2s ease-out;
+`;
+
+const SubjectName = styled.div`
+    width: 100%;
+    height: 30px;
     align-items: center;
+    display: flex;
+    padding-left: 10px;
+    border-radius: 2px;
+    cursor: pointer;
+    > p {
+        font-size: 14px;
+    }
+    &:hover {
+        background-color: #ECF7FF;
+    }
 `;
